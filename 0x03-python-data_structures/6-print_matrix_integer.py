e7527f640587ad33fafbc8fbe045b46498ba3dd7
Irakoze Darcy <irdondarcy33@gmail.com>
2023-05-11 02:26:41 -0700

    Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """function that prints a matrix of integers
+    """
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][column]), end='')
+        print()
